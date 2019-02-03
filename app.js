07d60a018be7c2efa13cf8b9a94a435e99941d5a
Amit <amitzmn@gmail.com>
2019-02-03 18:18:31 -0500

    Appjs: Created createColorBoxes and moved code inside

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const $colorBox = $('#color-box');
 const $myColorBox = $('#my-color-box');
 const $colorChanger = $('#color-changer');
 
+const createColorBoxes = () => {
+  $boxes = $('<div>').addClass('boxes');
+  $colorBox.append($boxes);
+};
+
 
 $(() => {
 
-  $boxes = $('<div>').addClass('boxes');
-  $colorBox.append($boxes);
+  createColorBoxes();
 
 });
