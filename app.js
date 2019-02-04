7e1e0c991e40dec2b9a82cb57474c50b920afa9c
Amit <amitzmn@gmail.com>
2019-02-03 21:09:49 -0500

    Can now Hover and add color to right box

--- app.js
+++ app.js
@@ -1,21 +1,38 @@
 const $colorBox = $('#color-box');
 const $myColorBox = $('#my-color-box');
-const $colorChanger = $('#color-changer');
+const $colorChanger = $('#color-changerBtn');
 
-const createColorBoxes = (numOfBoxes) => {
+const createColorBoxes = () => {
   $colorBox.empty();
-  for (let i = 0; i < numOfBoxes; i++) {
+  for (let i = 0; i < 112; i++) {
     const $box = $('<div>');
     $box.addClass('box');
 
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    $box.css('background-color', randomColor);
+    // $box.on('click', addColors);
+    $box.on('mouseover', addColors);
 
     $colorBox.append($box);
-  };
+  }
 };
+
+const addColors = (event) => {
+  const theColor = $(event.currentTarget).css('background-color');
+
+  const $box = $('<div>');
+  $box.addClass('box');
+  $box.css('background-color', theColor);
+
+  $myColorBox.append($box);
+};
+
+$colorChanger.on('click', createColorBoxes);
+
+
 
 $(() => {
 
-  createColorBoxes(200);
+  createColorBoxes();
 
 });
