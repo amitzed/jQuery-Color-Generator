6530463f5f22707479facd1cf239966e2b155ef7
Amit <amitzmn@gmail.com>
2019-02-03 20:19:50 -0500

    app.js: Randomizing box colors

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const $colorBox = $('#color-box');
 const $myColorBox = $('#my-color-box');
 const $colorChanger = $('#color-changer');
 
-const createColorBoxes = () => {
-  for (let i = 0; i < 200; i++) {
+const createColorBoxes = (numOfBoxes) => {
+  for (let i = 0; i < numOfBoxes; i++) {
     const $box = $('<div>');
     $box.addClass('box');
+
+    const randomColor = () => {
+      const red = Math.floor(Math.random() * 256);
+      const green = Math.floor(Math.random() * 256);
+      const blue = Math.floor(Math.random() * 256);
+      return "rgb("+ red +", "+ green +", "+ blue +")";
+    };
 
     $colorBox.append($box);
   };
 };
 
-
 $(() => {
 
-  createColorBoxes();
+  createColorBoxes(200);
 
 });
